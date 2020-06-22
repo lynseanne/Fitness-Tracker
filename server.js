6d71e9d8c16b65464b58021e140b1567a98f3be1
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-06-22 19:44:26 -0400

    added comments and renamed const values on  lines 17 & 18

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+//server framework to build single/multi page web applications
 const express = require("express");
+//node module for mongodb that emulates the official mongodb API 
 const mongojs = require("mongojs");
+//node mod for error tracking 
 const logger = require("morgan");
 
 const app = express();
@@ -11,8 +14,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const databaseUrl = "notetaker";
-const collections = ["notes"];
+const databaseUrl = "fitnessTracker";
+const collections = ["fitness"];
 
 const db = mongojs(databaseUrl, collections);
 
