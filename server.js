954016d757b0c8288ccd5b3356b64f7de0f4d701
Lindsey Lawson <lindseylawson@Lindseys-MBP.attlocal.net>
2020-06-22 19:31:40 -0400

    added files from develop folder & started server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "notetaker";
+const collections = ["notes"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.send(index.html);
+});
