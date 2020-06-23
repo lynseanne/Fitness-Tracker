34c3aac8edcadd67c227820787b47b8246787642
Lindsey Lawson <lynseahoss@gmail.com>
2020-06-23 19:47:22 -0400

    created workout schema

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    
+
+  day:{ type: Date, default: Date.now },
+  type:{ type:String, trim: true },
+  name:{ type:String, trim:true, default:"Yoga" },
+  duration:{ type: Number, min: 5, index: true },
+  weight:{ type: Number, min: 1, index: true },
+  reps:{ type: Number, min: 1, index: true } ,
+  sets:{ type: Number, min: 1, index: true } ,
+
   });
   
   const Workout = mongoose.model("Workout", WorkoutSchema);
