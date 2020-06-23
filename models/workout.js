6461fab982012dd613550d0b4cea5755604b5e7e
Lindsey Lawson <lynseahoss@gmail.com>
2020-06-22 22:29:34 -0400

    created workout.js in models folder

---
+++ models/workout.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    
+  });
+  
+  const Workout = mongoose.model("Workout", WorkoutSchema);
+  
+  module.exports = Workout;
+  
