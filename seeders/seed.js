a5a5c46fc63a44eb194057d8cde254876329fd4b
Lindsey Lawson <lynseahoss@gmail.com>
2020-06-24 18:30:24 -0400

    addded process.env.Mongogdb_uri to seeds

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 let workoutSeed = [
   {
