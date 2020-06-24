b27621139b0490d082d7dabc04fd1b906425da75
Lindsey Lawson <lynseahoss@gmail.com>
2020-06-23 20:07:31 -0400

    updated schema by adding default & required messages

--- models/workout.js
+++ models/workout.js
@@ -5,16 +5,17 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
 
   day:{ type: Date, default: Date.now },
-  type:{ type:String, trim: true },
-  name:{ type:String, trim:true, default:"Yoga" },
-  duration:{ type: Number, min: 5, index: true },
-  weight:{ type: Number, min: 1, index: true },
-  reps:{ type: Number, min: 1, index: true } ,
-  sets:{ type: Number, min: 1, index: true } ,
-
+  exercises:[
+  {type:{ type:String, trim: true, default: "Cardio", required: "Enter your excercise type" }},
+  {name:{ type:String, trim: true, default:"Yoga", required: "Enter your excercise" }},
+  {duration:{ type: Number, min: 5, index: true, required: "Enter minutes worked out" }},
+  {weight:{ type: Number, min: 1, index: true }},
+  {reps:{ type: Number, min: 1, index: true } },
+ { sets:{ type: Number, min: 1, index: true }} ,
+  ]
   });
   
-  const Workout = mongoose.model("Workout", WorkoutSchema);
+  const Workouts = mongoose.model("Workout", WorkoutSchema);
   
-  module.exports = Workout;
+  module.exports = Workouts;
   
