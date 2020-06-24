4568b38dd60c53b2df7bb4c71c81ca90ba861fd8
Lindsey Lawson <lynseahoss@gmail.com>
2020-06-24 00:20:41 -0400

    added api/workouts/range & api/workouts/:id

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -4,7 +4,7 @@ const db = require("../models")
 module.exports = function (app){
 
 
-  //get all workouts
+  //display all workouts
   app.get("/api/workouts", (req,res)=>{
     db.Workout.find({})
     .then(dbWorkout => {
@@ -15,7 +15,7 @@ module.exports = function (app){
     });
   })
 
-  //post all workouts ---> {body} is to post entire body 
+  //post request for new workout
   app.post("/api/workouts", ({body},res)=>{
     db.Workout.create({body})
     .then(dbWorkout => {
@@ -26,24 +26,25 @@ module.exports = function (app){
     });
   })
   
+  //display workout range
   app.get("/api/workouts/range", (req,res)=>{
     db.Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+    .then(dbWorkoutRange => {
+      res.json(dbWorkoutRange);
     })
     .catch(err => {
       res.json(err);
     });
   })
 
-
-  app.put("/api/workouts", ({body},res)=>{
-    db.Workout.create({body})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+ // Respond to a PUT request to the /api/workouts route
+  app.put("/api/workouts/:id", (req,res)=>{
+    db.Workout.updateOne({_id: req.params.id},
+        { $push: { exercise: req.body }, $inc:{totalDuration: req.body.duration}}, 
+        (err,dbWorkoutUpdate)=>{
+        if (err) res.status(418).json(err)
+        res.json(dbWorkoutUpdate)
     })
-    .catch(err => {
-      res.json(err);
-    });
+    
   })
 }
