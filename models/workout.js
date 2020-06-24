a89911f8bd455eeefc04aa3972f9d37496250c32
Lindsey Lawson <lynseahoss@gmail.com>
2020-06-23 20:50:04 -0400

    removed some data on schema

--- models/workout.js
+++ models/workout.js
@@ -6,16 +6,16 @@ const WorkoutSchema = new Schema({
 
   day:{ type: Date, default: Date.now },
   exercises:[
-  {type:{ type:String, trim: true, default: "Cardio", required: "Enter your excercise type" }},
-  {name:{ type:String, trim: true, default:"Yoga", required: "Enter your excercise" }},
-  {duration:{ type: Number, min: 5, index: true, required: "Enter minutes worked out" }},
-  {weight:{ type: Number, min: 1, index: true }},
-  {reps:{ type: Number, min: 1, index: true } },
- { sets:{ type: Number, min: 1, index: true }} ,
+  {type:{ type:String, trim: true, required: "Enter your excercise type" }},
+  {name:{ type:String, trim: true, required: "Enter your excercise" }},
+  {duration:{ type: Number, min: 5, required: "Enter minutes worked out" }},
+  {weight:{ type: Number, min: 1}},
+  {reps:{ type: Number, min: 1} },
+ { sets:{ type: Number, min: 1}} ,
   ]
   });
   
-  const Workouts = mongoose.model("Workout", WorkoutSchema);
+  const Workout = mongoose.model("Workout", WorkoutSchema);
   
-  module.exports = Workouts;
+  module.exports = Workout;
   
